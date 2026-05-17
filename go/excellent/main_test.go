ae6abd2f7e80e4bb047c0dc4be70f5a5648ef183
Yuduru <yud.namekawa@gmail.com>
2026-05-17 18:37:48 +0900

    Add test.yml

--- go/excellent/main_test.go
+++ go/excellent/main_test.go
@@ -2,8 +2,8 @@ package main
 
 import "testing"
 
-func TestEven0r0dd(t *testing.T) {
-	result := Even0r0dd(10)
+func TestEvenOrOdd(t *testing.T) {
+	result := EvenOrOdd(10)
 	if result != "Even" {
 		t.Errorf("Expected Even, actual: %s", result)
 	}
